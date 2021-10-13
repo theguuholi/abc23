33d73d10520a5ff639f6c09f5fb791c4d617eefe
Gustavo <g.92oliveira@gmail.com>
2021-10-13 09:56:20 -0300

    handle with events

--- assets/js/pickings/pickings-map.js
+++ assets/js/pickings/pickings-map.js
@@ -3,8 +3,17 @@ import LocationMap from "./location-map";
 
 const PickingsMap = {
     mounted() {
-        this.map = new LocationMap(this.el, [-23.585289979339343, -46.67357717366849], event => {
-            
+        let store = JSON.parse(this.el.dataset.selectedStore)
+        let location;
+        if (store == undefined) {
+            location =  [-23.585289979339343, -46.67357717366849];
+        } else {
+            location =  [store.lat, store.lng];
+        }
+
+        this.map = new LocationMap(this.el, location, event => {
+          const store_id = event.target.options.storeId
+          this.pushEvent("store-clicked", store_id)
         })
 
         const stores = JSON.parse(this.el.dataset.storePickingList)
--- lib/picking_and_delivery_web/live/pickings/picking_live.ex
+++ lib/picking_and_delivery_web/live/pickings/picking_live.ex
@@ -3,8 +3,32 @@ defmodule PickingAndDeliveryWeb.PickingLive do
   alias PickingAndDelivery.Pickings
 
   def mount(_assigns, _session, socket) do
-    socket = assign(socket, stores: Pickings.all_stores_with_pickings())
     {:ok, socket}
   end
 
+  defp select_store(stores, id) do
+    if id == nil do
+      nil
+    else
+      Enum.find(stores, &(&1.id == id))
+    end
+  end
+
+  def handle_params(params, _, socket) do
+    stores = Pickings.all_stores_with_pickings()
+    selected_store = select_store(stores, params["id"])
+    socket = assign(socket, stores: stores, selected_store: selected_store)
+    {:noreply, socket}
+  end
+
+  def handle_event("store-clicked", id, socket) do
+    selected_store = select_store(socket.assigns.stores, id)
+
+    socket =
+      socket
+      # |> assign(selected_store: selected_store)
+      # |> push_patch(to: "/pickings?id=#{id}")
+
+    {:noreply, socket}
+  end
 end
