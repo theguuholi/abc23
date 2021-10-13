dd10c22045ff73b42fe92ff8b6295c65310f9ab1
Gustavo <g.92oliveira@gmail.com>
2021-10-13 09:12:56 -0300

    learning phoenix 1.6

--- lib/picking_and_delivery_web/live/stores/products_component.ex
+++ lib/picking_and_delivery_web/live/stores/products_component.ex
@@ -6,6 +6,37 @@ defmodule PickingAndDeliveryWeb.ProductsComponent do
     {:ok, assign(socket, assigns)}
   end
 
+  def product(assigns) do
+    product = assigns.product
+    target = assigns.target
+    ~H"""
+    <li id={"product-" <> product.id}>
+      <p><%= product.name %></p>
+      <strong>Entregue?: </strong>
+      <.picking_status product={product} target={target} />
+    </li>
+    """
+  end
+
+  def picking_status(assigns) do
+    product = assigns.product
+    target = assigns.target
+    ~H"""
+        <button id={"update-product-"<> product.id}
+                disabled={product.delivered}
+                phx-click="deliver"
+                phx-target={target}
+                phx-value-id={product.id}
+        >
+          <%= if product.delivered do %>
+          Entregue
+          <% else %>
+          Entregar
+          <% end %>
+        </button>
+    """
+  end
+
   def handle_event("deliver", %{"id" => id}, socket) do
     products = Pickings.update_picking(id, socket.assigns.products)
     {:noreply, update(socket, :products, fn _ -> products end)}
